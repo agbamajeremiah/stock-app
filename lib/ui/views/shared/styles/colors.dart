c3c855a053291afe38684cc321eb0083fa78e1be
Jeremiah Akem <agbamajeremiah@gmail.com>
2021-11-15 07:32:27 +0100

    add app color class

---
+++ lib/ui/views/shared/styles/colors.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primaryColor = Color(0xff03314B);
+  static const accent = Color(0xffD7EAE7);
+
+  static const background = Color(0xffE5E5E5);
+  static const white = Color(0xffFFFFFF);
+  static const red = Color(0xffDD0707);
+
+  // ignore: constant_identifier_names
+  static const Map<int, Color> COLOR_SCRATCH = {
+    50: Color.fromRGBO(7, 45, 114, .1),
+    100: Color.fromRGBO(7, 45, 114, .2),
+    200: Color.fromRGBO(7, 45, 114, .3),
+    300: Color.fromRGBO(7, 45, 114, .4),
+    400: Color.fromRGBO(7, 45, 114, .5),
+    500: Color.fromRGBO(7, 45, 114, .6),
+    600: Color.fromRGBO(7, 45, 114, .7),
+    700: Color.fromRGBO(7, 45, 114, .8),
+    800: Color.fromRGBO(7, 45, 114, .9),
+    900: Color.fromRGBO(7, 45, 114, 1),
+  };
+}
