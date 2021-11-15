49df255aae8c6953fa4afdefa876e5f94af4c757
Jeremiah Akem <agbamajeremiah@gmail.com>
2021-11-15 07:59:24 +0100

    Register app assets

--- lib/core/constants/app_asset.dart
+++ lib/core/constants/app_asset.dart
@@ -1,1 +1,11 @@
-class AppAsset {}
+class AppAsset {
+  static const String accountActiveIcon = 'asets/icons/account_active.svg';
+  static const String accountInactiveIcon = 'asets/icons/account_inactive.svg';
+  static const String homeInactiveIcon = 'asets/icons/home_inactive.svg';
+  static const String homeActiveIcon = 'asets/icons/home_active.svg';
+  static const String mapActiveIcon = 'asets/icons/map_active.svg';
+  static const String marketActiveIcon = 'asets/icons/market_active.svg';
+  static const String marketInactiveIcon = 'asets/icons/market_inactive.svg';
+  static const String tradeActiveIcon = 'asets/icons/trade_active.svg';
+  static const String stockAnimation = 'asets/gifs/stock_anim.gif';
+}
